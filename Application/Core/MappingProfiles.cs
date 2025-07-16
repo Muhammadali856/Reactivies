74cf94758566a99730069a9e005cbe7af7c442dc
MUHAMMADALI-USER <eshboevmuhammadali2@gmail.com>
2025-07-16 14:51:46 +0500

    End of section 4

---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using AutoMapper;
+
+
+namespace Application.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Activity, Activity>();
+        }
+    }
+}
